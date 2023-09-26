7f5779d82c71198b0ebf2ebe0f7163df0d217847
AbekM <asilbek.makhmudov@outlook.com>
2023-09-26 23:09:33 +0500

    dozno

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null;
+        Integer age = null;
+        Long phone = null;
+        String email = null;
+        String tmp;
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br;
+        try (java.io.FileReader fr = new java.io.FileReader(file)) {
+            br = new BufferedReader(fr);
+            while ((tmp = br.readLine()) != null) {
+                sb.append(tmp).append(";");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tmp = sb.toString();
+        String[] response = tmp.split(";");
+        for (String s : response) {
+            if (s.startsWith("Name")) {
+                name = s.substring(6);
+            } else if (s.startsWith("Age")) {
+                age = Integer.parseInt(s.substring(5));
+            } else if (s.startsWith("Email")) {
+                email = s.substring(7);
+            } else if (s.startsWith("Phone")) {
+                phone = Long.parseLong(s.substring(7));
+            }
+        }
+        return new Profile(name, age, email, phone);
     }
 }
